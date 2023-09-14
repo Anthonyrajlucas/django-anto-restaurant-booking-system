3b5f735423375e74b1c53871c12e413f6081d62a
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-09-14 20:36:09 +0000

    heroku and database added

--- anto_restaurant/settings.py
+++ anto_restaurant/settings.py
@@ -15,11 +15,12 @@ import os
 import dj_database_url
 
 if os.path.isfile("env.py"):
-   import env
+    import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -28,11 +29,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 
 ALLOWED_HOSTS = [ '8000-anthonyrajl-djangoantor-qmdhqu4cavz.ws-eu104.gitpod.io',
-                  "django-anto-restaurant-booking.herokuapp.com",
-                  "localhost" ]
+                 'django-anto-restaurant-booking-a13bc07eab32.herokuapp.com' ,
+                 "localhost" ]
 
 
 # Application definition
@@ -63,8 +65,8 @@ ROOT_URLCONF = 'anto_restaurant.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'BACKEND' : 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [ TEMPLATES_DIR ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
