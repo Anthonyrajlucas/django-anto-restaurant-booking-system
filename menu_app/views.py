7021dbdaaf7294818193a8b1e561dcff45bd45c4
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-09-18 18:55:38 +0000

    Create class listview and createview in view.py

--- menu_app/views.py
+++ menu_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import MenuItem
+from .forms import MenuItemForm
+from django.views.generic import ListView, CreateView
 
-# Create your views here.
+
+
+class MenuListView(ListView):
+    model = MenuItem
+    template_name = 'menu_list.html'
+    context_object_name = 'menu_items'
+
+
+class MenuCreateView(CreateView):
+    model = MenuItem
+    form_class = MenuItemForm
+    template_name = 'menu_form.html'
