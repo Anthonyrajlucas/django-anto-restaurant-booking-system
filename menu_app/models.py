6b2d1ecbfaa9b61a25debcaccd2a9cd03fcc4219
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-09-18 19:13:45 +0000

    Create fuction menuitems in models.py

--- menu_app/models.py
+++ menu_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class MenuItem(models.Model):
+      menu_id = models.AutoField(primary_key=True)
+      menu_name = models.CharField(max_length=100)
+      description = models.TextField()
+      category = models.CharField(max_length=50)
+      price = models.DecimalField(max_digits=8, decimal_places=2)
+      image_url = models.URLField()
+      
+      def __str__(self):
+          return self.menu_name
