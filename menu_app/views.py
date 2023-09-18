69eb1d37c0e30fc92d8981addc06d3549397a2ed
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-09-18 20:05:54 +0000

    migrated menu database

--- menu_app/views.py
+++ menu_app/views.py
@@ -3,13 +3,10 @@ from .models import MenuItem
 from .forms import MenuItemForm
 from django.views.generic import ListView, CreateView
 
-
-
 class MenuListView(ListView):
     model = MenuItem
     template_name = 'menu_list.html'
     context_object_name = 'menu_items'
-
 
 class MenuCreateView(CreateView):
     model = MenuItem
