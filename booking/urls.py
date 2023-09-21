e46ef4efdd6a1ecc2376d269fd07540b2d4197de
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-09-21 19:05:58 +0000

    Path added in urls.py

--- booking/urls.py
+++ booking/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BookingListView
+from .views import BookingListView, BookingCreateView
 
 urlpatterns = [
     path('bookings/', BookingListView.as_view(), name='booking_list'),
+     path('bookings/new/', BookingCreateView.as_view(), name='booking-create'),
 ]
