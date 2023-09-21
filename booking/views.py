1d5bd45e58bee6b5b3bd2a379e7cbe9b108b8928
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-09-21 18:13:21 +0000

    In view.py class defined bookinf list view

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView
+from .models import Booking
 
-# Create your views here.
+
+class BookingListView(LoginRequiredMixin, ListView):
+    model = Booking
+    template_name = 'booking/booking_list.html'
+    context_object_name = 'bookings'
+    ordering = ['-created_on']
+
+
