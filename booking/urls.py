8d15c4003ca93fe2d7c8efa3b6d709b433ba7d97
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-09-21 19:13:23 +0000

    Updateview urls pattern set in urls.py

--- booking/urls.py
+++ booking/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BookingListView, BookingCreateView
+from .views import BookingListView, BookingCreateView, BookingUpdateView
 
 urlpatterns = [
     path('bookings/', BookingListView.as_view(), name='booking_list'),
     path('bookings/new/', BookingCreateView.as_view(), name='booking-create'),
+    path('bookings/<int:pk>/update/', BookingUpdateView.as_view(), name='booking-update'),
 ]
