6b9799f4846cc759447637b4878c2da66dcbf012
Anthonyrajlucas <anthonyraj.lucas@gmail.com>
2023-09-21 18:02:06 +0000

    models.py, define the Booking model with the specified fields

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Booking(models.Model):
+    booking_id = models.AutoField(primary_key=True)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    customer_name = models.CharField(max_length=255)
+    customer_email = models.EmailField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Booking ID: {self.booking_id}, Date: {self.date}"
